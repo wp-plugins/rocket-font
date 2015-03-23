8431bfcd173f23be034bde74a0dd7655829d8751
Qwerty_23 <Qwerty_23@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-23 05:49:04 +0000

    0.0.2
    git-svn-id: https://plugins.svn.wordpress.org/rocket-font/tags/0.0.2@1118464 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/templates/admin/tinymce-rocketfont-dynamic-css.php
@@ -0,0 +1,25 @@
+<?php
+$double_qute = '';
+if(strpos($selected_font_info['font_name'], ' ') > 0){
+	$double_qute = '"';
+}
+?>
+
+<?php
+foreach($target_tag_list as $target_tag):
+	
+	if($options['font_use_'.$target_tag]=="yes"):
+		echo $target_tag;?>
+			{
+			font-family: <?php if($selected_backup_font_info['font_name']) { echo $selected_backup_font_info['font_name'] . ",";}?> <?php echo $double_qute . $selected_font_info['font_name'] . $double_qute?>, <?php if($selected_backup_font_info['generic_font_family']) { echo $selected_backup_font_info['generic_font_family'];}else{ echo "sans-serif";}?>;
+			<?php
+			if($options['font_color_'.$target_tag]){ ?> color:<?php echo $options['font_color_'.$target_tag]?>;<?php } 
+			if($options['font_weight_'.$target_tag]){ ?> font-size:<?php echo $options['font_weight_'.$target_tag]?>px;<?php } 
+			if($options['font_lineheight_'.$target_tag]){ ?> line-height:<?php echo $options['font_lineheight_'.$target_tag]?>px; <?php } 
+			?> 
+			}
+		<?php
+	endif;
+	
+endforeach;
+?>
